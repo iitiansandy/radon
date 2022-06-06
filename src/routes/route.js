e582e77f4930698f156e647f3392bfa957237a5c
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-06-06 21:29:21 +0530

    mongo session1 assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,8 +3,23 @@ const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
+router.post("/test-post", function (req, res) {
+
+
+    // let id=req.body.user;
+    // let pwd = req.body.password;
+
+    // console.log(id, pwd)
+    console.log( req.body )
+    res.send({msg: "hi", status: true})
+})
+
+router.post("/test-post-2", function(req, res){
+    let arr = [12, "functionUp"];
+    let ele = req.body.element;
+    arr.push(ele)
+
+    res.send( {msg: arr, status: true} )
 })
 
 router.post("/createUser", UserController.createUser  )
