bbc0e30bf7e72abbb1287d6ce484317e3383df80
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-06-07 16:54:52 +0530

    mongo session2 part2 assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,11 +8,17 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createBooks", BookController.createBook  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.post("/getParticularBooks", BookController.getParticularBooks)
 
-router.post("/createBook", BookController.createBook  )
+router.post("/getBookInYear", BookController.getBookInYear  )
+
+router.get("/bookList", BookController.bookList)
+
+router.get("/getXINRBooks", BookController.getXINRBooks)
+
+router.get("/getRandomBooks", BookController.getRandomBooks)
 
 router.get("/getBooksData", BookController.getBooksData)
 
